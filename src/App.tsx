db8ede9587feb4005adba55610895c29f06c0bfa
mihaelapopovic <mihaela@customised.cloud>
2023-06-05 07:25:02 +0200

    Main components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./layouts/navbar/Navbar";
+import Homepage from "./pages/homepage/Homepage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Homepage />
+    </>
+  );
+}
+
+export default App;
